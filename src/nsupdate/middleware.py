fce0e3d72c1ff72e58876ca3530f10efcedf7b43
stevo <stefan.dlugolinsky@savba.sk>
2025-04-28 13:45:43 +0200

    middleware for real IP - usefull when nsupdate is behind proxy

--- src/nsupdate/middleware.py
+++ src/nsupdate/middleware.py
@@ -50,3 +50,14 @@ class BearerTokenMiddleware:
         # Proceed with the request processing.
         response = self.get_response(request)
         return response
+
+
+class RealIPMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            request.META['REMOTE_ADDR'] = x_forwarded_for.split(',')[0].strip()
+        return self.get_response(request)

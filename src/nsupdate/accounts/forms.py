7629cb78a33c9311036150566060a8cea3b4a02d
stevo <stefan.dlugolinsky@savba.sk>
2025-04-28 13:44:16 +0200

    show username in profile

--- src/nsupdate/accounts/forms.py
+++ src/nsupdate/accounts/forms.py
@@ -9,10 +9,14 @@ from .models import UserProfile
 class UserForm(forms.ModelForm):
     class Meta(object):
         model = get_user_model()
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email']
         widgets = {
             'email': forms.widgets.TextInput(attrs=dict(autofocus=None)),
         }
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].disabled = True
+        self.fields['username'].help_text = None
 
 
 class UserProfileForm(forms.ModelForm):

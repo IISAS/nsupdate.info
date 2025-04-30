fc6c8b773da6dcf8e1ba76c53b5c84861693a34d
stevo <stefan.dlugolinsky@savba.sk>
2025-04-30 11:00:32 +0200

    Bitbucket OAuth1 to OAuth2

--- src/nsupdate/settings/base.py
+++ src/nsupdate/settings/base.py
@@ -305,7 +305,7 @@ PASSWORD_HASHERS = [
 # python-social-auth settings
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.amazon.AmazonOAuth2',
-    'social_core.backends.bitbucket.BitbucketOAuth',
+    'social_core.backends.bitbucket.BitbucketOAuth2',
     'social_core.backends.disqus.DisqusOAuth2',
     'social_core.backends.dropbox.DropboxOAuth2V2',
     'social_core.backends.github.GithubOAuth2',
